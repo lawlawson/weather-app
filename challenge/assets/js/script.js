15399cf7ac990a85ec4aeb622340a76fc2f20d59
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2025-02-05 18:13:49 +0000

    managed to get data to display dynamically

--- challenge/assets/js/script.js
+++ challenge/assets/js/script.js
@@ -1,28 +1,54 @@
 const API_KEY = 'f23ee9deb4e1a7450f3157c44ed020e1';
 
-// First, get the latitude and longitude for the city
-const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?${city}&limit=1&appid=${API_KEY}`;
+function getWeather() {
+  const city = 'London';
 
-let lat = 51.5073219;
-let lon = -0.1276474;
+  const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
 
-// Call getWeather API when the button is clicked
-const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  
+  // Fetching data from api
+  fetch(geoUrl)
+    .then((response) => response.json())
+    .then((geoData) => {
+      if (geoData.length === 0) {
+        throw new Error('City not found');
+      }
+      const lat = geoData[0].lat; //51.5073219;
+      const lon = geoData[0].lon; //-0.1276474;
 
-// Fetching data from api
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-fetch(weatherUrl)
-  .then((response) => response.json())
+      return fetch(weatherUrl);
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      const cityName = data.name;
+      const cityTemp = data.main.temp;
+      const cityDescription = data.weather[0].description;
 
-  .then((data) => {
-    console.log(data);
-    console.log(data.name, data.main.temp, data.weather[0].description); // Access specific weather data
-  })
+      displayData(cityName, cityTemp, cityDescription);
+    })
 
-  .catch((error) => console.error('Error fetching weather:', error));
-
-// Function to display data in HTML file
-function displayData(data) {
-  const container = document.getElementById('weatherResult');
-  container.innerHTML = '';
+    .catch((error) => console.error('Error fetching weather:', error));
+  displayError('Failed to get weather info. Try again');
 }
+
+// Function to display data in HTML
+function displayData(cityName, cityTemp, cityDescription) {
+  const container = document.getElementById('weatherResult');
+
+  container.innerHTML = '';
+
+  const dataElement = document.createElement('div');
+  dataElement.classList.add('weather-info');
+
+  dataElement.innerHTML = `
+      <p>City: ${cityName}</p>
+      <p>Temperature: ${cityTemp}</p>
+      <p>Description: ${cityDescription}</p>
+    `;
+
+  container.appendChild(dataElement);
+}
+
+document.getElementById('getWeather').addEventListener('click', getWeather);
